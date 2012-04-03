50030bf662651d33cdbb8232220928233595706d
Karthikeyan A K <mindaslab@gmail.com>
2012-04-03 22:13:43 +0530

    question and answer framework added

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,35 @@
 class AnswersController < ApplicationController
+  layout 'questions'
+  before_filter :find_qua, :except=>[:create]
   def create
     @question = Question.find(params[:question_id])
     @answer = @question.answers.create(params[:answer])
     redirect_to question_path(@question)
   end
+  
+  def destroy
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    @answer.destroy
+    redirect_to @question, notice: "Answer Deleted"
+  end
+  
+  def edit
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+  end
+  
+  def update
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    @answer.update_attributes params[:answer]
+    @answer.save
+    redirect_to @question, notice: "Answer Updated"
+  end
+  
+  private
+  def find_qua
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+  end    
 end
