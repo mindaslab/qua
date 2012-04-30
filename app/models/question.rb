b6ddc7c8fa556ccb2b88384a58ab1931f642a70a
Karthikeyan A K <mindaslab@gmail.com>
2012-04-30 14:32:06 +0530

    work for tags started

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,15 @@
 class Question < ActiveRecord::Base
+  attr_accessor :tag_string
   attr_protected :user_id
   validates_presence_of :content, :message => " of question should not be blank."
   has_many :answers, :dependent => :destroy
   has_and_belongs_to_many :tags
   belongs_to :user
+  
+  def tag_string=(string)
+    tag_names = string.split(',').collect{|x| x.gsub(/\s+/, " ").strip.downcase}
+    for tag_name in tag_names
+      self.tags << Tag.find_or_create_by_name(tag_name)
+    end
+  end
 end
